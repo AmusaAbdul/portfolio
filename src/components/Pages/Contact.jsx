4dd7006a89ae1599ea2290cd7113d00cb621dd20
AmusaAbdul <amusaabdullah1234@gmail.com>
2026-01-11 21:03:37 +0100

    Update contact form Logic

--- src/components/Pages/Contact.jsx
+++ src/components/Pages/Contact.jsx
@@ -2,23 +2,23 @@ import { useState } from "react";
 
 function Contact() {
 
-    const [Name, setName] = useState("")
+    const [name, setName] = useState("")
 
     function handleSubmit(event) {
         event.preventDefault()
-    }
-    
-    const trimmedName = Name.trim()
 
-    if (trimmedName) {
-        alert("Wagwan " + trimmedName + " Your message has been sent")
+        const trimmedName = name.trim()
+
+        if (trimmedName) {
+            alert("Wagwan " + trimmedName + " Your message has been sent")
+        }
     }
     return(
         <section className="contact container card">
             <h2>Contact Me</h2>
             <form className="contact-form" onSubmit={handleSubmit}>
                 <label htmlFor="name">Name</ label>
-                <input type="text" className="name" required placeholder="Enter your name" onChange={(event) => setName(event.target.value)} />
+                <input type="text" className="name" required placeholder="Enter your name" value={name} onChange={(event) => setName(event.target.value)} />
 
                 <label htmlFor="email">Email</label>
                 <input type="text" className="email" required placeholder="Enter your email" />
