e98b125e71ee58749b7ab1ea95921a5028662964
AmusaAbdul <amusaabdullah1234@gmail.com>
2026-01-11 17:19:36 +0100

    Final build

--- src/components/Pages/Contact.jsx
+++ src/components/Pages/Contact.jsx
@@ -1,10 +1,24 @@
+import { useState } from "react";
+
 function Contact() {
+
+    const [Name, setName] = useState("")
+
+    function handleSubmit(event) {
+        event.preventDefault()
+    }
+    
+    const trimmedName = Name.trim()
+
+    if (trimmedName) {
+        alert("Wagwan " + trimmedName + " Your message has been sent")
+    }
     return(
         <section className="contact container card">
             <h2>Contact Me</h2>
-            <form className="contact-form">
+            <form className="contact-form" onSubmit={handleSubmit}>
                 <label htmlFor="name">Name</ label>
-                <input type="text" className="name" required placeholder="Enter your name" />
+                <input type="text" className="name" required placeholder="Enter your name" onChange={(event) => setName(event.target.value)} />
 
                 <label htmlFor="email">Email</label>
                 <input type="text" className="email" required placeholder="Enter your email" />
